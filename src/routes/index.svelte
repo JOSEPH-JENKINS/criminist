32b70c9a681385a8a1b2075ce711e382b8e35250
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-05-20 20:53:26 +0100

    first commit

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -7,18 +7,79 @@
 	<title>Document</title>
 </head>
 <body class="bg-black text-white overflow-x-hidden">
-	<main class="h-screen w-screen sm:px-12 grid grid-cols-12">
-		<div class="head-text mt-10 mb-10 sm:col-span-12">
-			<h1 class="font-display sm:text-9xl uppercase text-center">Criminist-justice.</h1>
+	<main class="h-screen w-screen px-4 sm:px-12 grid grid-cols-4 sm:grid-cols-12">
+		<div class="head-text mt-10 mb-10 col-span-4 sm:col-span-12">
+			<h1 class="font-display text-4xl sm:text-9xl uppercase text-center">Criminist-justice.</h1>
 		</div>
-		<div class="head-image sm:col-span-12 h-[48rem] mb-10">
+		<div class="head-image col-span-4 sm:col-span-12 h-[48rem] mb-10">
 			<img src='/img/david-von-diemar-jM6Y2nhsAtk-unsplash.jpg' class="w-full h-full object-cover" alt="" />
 		</div>
-		<section class="sm:col-span-12 h-screen flex justify-center items-center">
+		<section class="sm:col-span-12 col-span-4 h-screen flex justify-center items-center">
 			<span class="font-reg uppercase sm:text-2xl w-3/4 text-center">
 				When a man is denied the right to live the life he believes in, he has the choice but to become an outlaw.
 			</span>
 		</section>
+		<section class="sm:col-span-12 col-span-4 mb-5">
+			<span class="mb-5">
+				<h1 class="font-display text-4xl sm:text-9xl uppercase">Books.</h1>
+			</span>
+			<section id="books col-span-4 sm:col-span-2 flex flex-wrap">
+				<div class="book mt-5 h-[48rem] w-full relative">
+					<div class="card w-full h-1/2 sm:w-1/2 sm:h-3/4 absolute">
+						<div class="image w-full h-full mb-7">
+							<img src="/img/david-von-diemar-jM6Y2nhsAtk-unsplash.jpg" class="w-full h-full object-cover" alt="">
+						</div>
+						<div class="details w-full">
+							<h1 class="font-display text-lg mb-5">Lorem ipsum</h1>
+							<p class="font-reg text-sm mb-5">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+								Tempore ab vero recusandae totam nostrum consectetur,
+								asperiores at laborum ducimus porro iure quaerat alias excepturi et,
+								tempora soluta provident facere expedita!
+							</p>
+							<button href="#" class="w-24 hover:bg-red-400 bg-red-700"><span class="text-xs font-display">Buy</span></button>
+						</div>
+					</div>
+				</div>
+				<div class="book mt-5 h-[48rem] w-full relative">
+					<div class="card w-full h-1/2 sm:w-1/2 sm:h-3/4 absolute sm:right-0">
+						<div class="image w-full h-full mb-7">
+							<img src="/img/david-von-diemar-jM6Y2nhsAtk-unsplash.jpg" class="w-full h-full object-cover" alt="">
+						</div>
+						<div class="details w-full">
+							<h1 class="font-display text-lg mb-5">Lorem ipsum</h1>
+							<p class="font-reg text-sm mb-5">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+								Tempore ab vero recusandae totam nostrum consectetur,
+								asperiores at laborum ducimus porro iure quaerat alias excepturi et,
+								tempora soluta provident facere expedita!
+							</p>
+							<button href="#" class="w-24 hover:bg-red-400 bg-red-700"><span class="text-xs font-display">Buy</span></button>
+						</div>
+					</div>
+				</div>
+				<div class="book mt-5 h-[48rem] w-full relative">
+					<div class="card w-full h-1/2 sm:w-1/2 sm:h-3/4 absolute">
+						<div class="image w-full h-full mb-7">
+							<img src="/img/david-von-diemar-jM6Y2nhsAtk-unsplash.jpg" class="w-full h-full object-cover" alt="">
+						</div>
+						<div class="details w-full">
+							<h1 class="font-display text-lg mb-5">Lorem ipsum</h1>
+							<p class="font-reg text-sm mb-5">
+								Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+								Tempore ab vero recusandae totam nostrum consectetur,
+								asperiores at laborum ducimus porro iure quaerat alias excepturi et,
+								tempora soluta provident facere expedita!
+							</p>
+							<button href="#" class="w-24 hover:bg-red-400 bg-red-700"><span class="text-xs font-display">Buy</span></button>
+						</div>
+					</div>
+				</div>
+			</section>
+		</section>
+		<footer class="h-7 w-full col-span-4 sm:col-span-12 flex justify-center items-center mb-5">
+			<p class="font-display text-base">&copy; 2022</p>
+		</footer>
 	</main>
 </body>
 </html>
