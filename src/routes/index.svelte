615b6cf9c32214846c9f2ce44f80e66ad5a35940
JOSEPH-JENKINS <amjosephjenkins@gmail.com>
2022-05-20 12:52:27 +0100

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+</head>
+<body class="bg-black text-white overflow-x-hidden">
+	<main class="h-screen w-screen sm:px-12 grid grid-cols-12">
+		<div class="head-text mt-10 mb-10 sm:col-span-12">
+			<h1 class="font-display sm:text-9xl uppercase text-center">Criminist-justice.</h1>
+		</div>
+		<div class="head-image sm:col-span-12 h-[48rem] mb-10">
+			<img src='/img/david-von-diemar-jM6Y2nhsAtk-unsplash.jpg' class="w-full h-full object-cover" alt="" />
+		</div>
+		<section class="sm:col-span-12 h-screen flex justify-center items-center">
+			<span class="font-reg uppercase sm:text-2xl w-3/4 text-center">
+				When a man is denied the right to live the life he believes in, he has the choice but to become an outlaw.
+			</span>
+		</section>
+	</main>
+</body>
+</html>
